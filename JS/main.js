f5abfc69edc6ed252ad332cba6d82d84b9aa85a7
minaMagdy50 <magdymina583@gmail.com>
2024-02-13 21:14:43 +0200

    final commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typed", {
+    strings: ["Designer", "Developer" , "freelancer" , "photographer" ],
+    typeSpeed: 35,
+    startDelay: 500, 
+    backDelay:1000,
+    loop: true,
+  });
